61692856d3c0e6a62fc2e06f15468e0588e59675
Unknown <moreno6230@gmail.com>
2018-04-27 11:28:53 -0300

    modificacao do exercicio

--- aula 1,2,3 e 4/Cap3-Ex13.py
+++ aula 1,2,3 e 4/Cap3-Ex13.py
@@ -3,29 +3,7 @@ wn = turtle.Screen()
 wn.bgcolor("lightgreen")
 andre = turtle.Turtle()
 
-andre.shape("turtle")
-andre.color("blue")
-andre.pensize(3)
-andre.stamp()
-andre.penup()
-size1 = 100
-size2 = 10
-size3 = 20
-size4 = size1 + size2 + size3
+type(andre)
 
-for i in range(12):
-    andre.forward(size1)
-    andre.pendown()
-    andre.forward(size2)
-    andre.penup()
-    andre.forward(size3)
-    andre.stamp()
-    andre.right(180)
-    andre.forward(size4)
-    andre.right(150)
-
-
-wn.mainloop()
-
-
+print(type(andre))
 
